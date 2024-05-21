057056cb5af564d4f2da8859d1b5624abcc56817
fuatX <fuatyagizaydin@hotmail.com>
2024-05-22 00:29:50 +0300

    added-card-styles-and-database

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,19 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
+
 export function Store() {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map((item, index) => (
+          <Col key={index}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
+// console.log(storeItems.map((item) => JSON.stringify(item)));
